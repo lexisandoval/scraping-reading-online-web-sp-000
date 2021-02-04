eff7666af75bb795a8ceabae2683aa77e98304af
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-02-04 21:11:08 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,4 +6,4 @@ html = open("https://flatironschool.com/")
 doc = Nokogiri::HTML(html)
 puts doc.css(".headline-26OIBN").text.strip
 
-puts doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
+puts doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G").text
